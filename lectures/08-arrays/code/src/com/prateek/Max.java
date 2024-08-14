1d655be6c3470ddea8ae3267aa79b7f3eeeef64f
Prateeksingchn <prateeksinghchouhan4499@gmail.com>
2024-08-14 20:55:04 +0530

    array lecture complete

--- lectures/08-arrays/code/src/com/prateek/Max.java
+++ lectures/08-arrays/code/src/com/prateek/Max.java
@@ -1,2 +1,38 @@
-package com.prateek;public class Max {
+package com.prateek;
+
+public class Max {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 23, 9, 18};
+//        System.out.println(max(arr));  //output:- 23
+        System.out.println(maxRange(arr, 1, 3));  //output:- 23
+    }
+    // finding maxVal in a Range
+    static int maxRange(int[] arr, int start, int end) {
+
+        if (end > start) {
+            return -1;
+        }
+        if (arr == null) {
+            return -1;
+        }
+
+        int maxVal = arr[0];
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+            }
+        }
+        return maxVal;
+    }
+    // Imagine that arr is not empty
+//    static int max(int[] arr) {
+//        int maxVal = arr[0];
+//        for (int i = 1; i < arr.length; i++) {
+//            if (arr[i] > maxVal) {
+//                maxVal = arr[i];
+//            }
+//        }
+//        return maxVal;
+//    }
+
 }
--- lectures/08-arrays/code/src/com/prateek/Swap.java
+++ lectures/08-arrays/code/src/com/prateek/Swap.java
@@ -1,2 +1,32 @@
-package com.prateek;public class Swap {
+package com.prateek;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 23, 9, 18};
+//        swap(arr, 1, 3); //output:- [1, 9, 23, 3, 18]
+//        swap(arr, 0, 4); //output:- [18, 3, 23, 9, 1]
+
+        reverse(arr); //output:- [18, 9, 23, 3, 1]
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start < end) {
+            //swap
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    static void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
 }
